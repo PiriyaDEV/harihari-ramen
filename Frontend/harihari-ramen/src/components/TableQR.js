53c53695438a25bcc4eba1f44eb0a57e6d15c547
Piriya Chaigul <pryeditor@gmail.com>
2021-10-19 00:30:02 +0700

    Add Home & Table Page

--- Frontend/harihari-ramen/src/components/TableQR.js
+++ Frontend/harihari-ramen/src/components/TableQR.js
@@ -1,54 +1,62 @@
-import React from "react";
+import React, { Component } from "react";
 
+//Service
+import tableService from "../services/table.service.js";
+
+//CSS
 import "../css/page.css";
-import "../css/TableQR.css"
-// import { Link } from "react-router-dom";
+import "../css/page/TableQR.css";
 
-export default function TableQR() {
-  const link = [
-    "table1uid",
-    "table2uid",
-    "table3uid",
-    "table4uid",
-    "table5uid",
-    "table6uid",
-  ];
+const link = ["2", "3", "4", "1", "10", "20"];
 
-  return (
-    <div id="table-qr" className="section">
-      <div className="page-container">
-        <h1>Test</h1>
-        {link.map((index) => (
-          <div key={index}>
-            <h1 onClick={() => openLink(index)}>{index}</h1>
-            <img className="qr-code" src={makeQR(index)} alt=""></img>
-          </div>
-        ))}
+export default class TableQR extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            link : [],
+        }
+    }
+
+  makeQR(value) {
+    let web = "http://localhost:3000/";
+    let qrlink =
+      "https://chart.googleapis.com/chart?cht=qr&chl=" +
+      web +
+      "table/" +
+      value +
+      "&chs=160x160&chld=L|0";
+
+    return qrlink;
+  }
+
+  openLink(value) {
+    let web = "http://localhost:3000/";
+    let path = web + "table/" + value;
+    window.location = path;
+  }
+
+  async getLink() {
+    return await tableService
+      .getTables()
+      .then((data) => this.setState( {link: data} ));
+  }
+  
+  render() {
+      
+    // let linked = this.state.link;
+    // console.log(this.state.link)
+    return (
+      <div id="table-qr" className="section">
+        <div className="page-container">
+          {link.map((index) => (
+            <div key={index}>
+              <h1 onClick={() => this.openLink(index)}>{index}</h1>
+              <img className="qr-code" src={this.makeQR(index)} alt=""></img>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-function makeQR(value) {
-  let web = "http://localhost:3000/";
-  let qrlink =
-    "https://chart.googleapis.com/chart?cht=qr&chl=" +
-    web +
-    "order/" +
-    value +
-    "&chs=160x160&chld=L|0";
-
-  return qrlink;
-}
-
-let openLink = (value) => {
-  let web = "http://localhost:3000/";
-  let path = web + "order/" + value;
-  window.location = path;
-};
-
-// function clickQR(value) {
-//   let web = "http://localhost:3000/";
-//   let path = web + "order?table=" + value;
-//   return window.location = path;
-// }
---
+++ Frontend/harihari-ramen/src/components/TestTable.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TestTable extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
