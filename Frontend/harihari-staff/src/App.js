29b970724df3b4a2226c65443fd003436bcc5c0a
Piriya Chaigul <pryeditor@gmail.com>
2021-11-25 15:06:07 +0700

    Add Custom Ramen Images

--- Frontend/harihari-staff/src/App.js
+++ Frontend/harihari-staff/src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 // import React from "react";
 import { Route, Switch } from "react-router-dom";
 
-import Receipt from "./components/Receipt";
 import Mainpage from "./components/Mainpage";
 
 function App() {
@@ -11,10 +10,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/">
-            <Mainpage />
-          </Route>
-        <Route path="/receipt">
-          <Receipt />
+          <Mainpage />
         </Route>
         {/* <Route path="/:id">
           <p>ไม่มี pathนี้</p>
